75fd0e4e949a482868a863ff19ec47ad06d90596
lvjunning124.015876 <lvjunning1243015876@gamil.com>
2019-08-12 19:19:44 +0800

    update xsens_ros_mti_driver

--- xsens_ros_mti_driver/lib/xspublic/xstypes/xsvector3.d
+++
@@ -1,9 +0,0 @@
-xsvector3.c.o: xsvector3.c xsvector3.h xsvector.h xsmath.h xstypesconfig.h \
- xstypedefs.h pstdint.h
-xsvector3.c:
-xsvector3.h:
-xsvector.h:
-xsmath.h:
-xstypesconfig.h:
-xstypedefs.h:
-pstdint.h:
